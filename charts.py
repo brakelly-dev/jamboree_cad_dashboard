802e0e0c22281748d45e4b9055188e0af7a813bf
brakelly-dev <eric.b.kelly@live.com>
2026-07-07 20:30:13 -0400

    we're almost there

--- charts.py
+++ charts.py
@@ -61,7 +61,7 @@ def transport_donut(motor_coach: int, personal_vehicle: int) -> go.Figure:
             orientation="h",
             yanchor="top",
             y=-0.08,
-            xanchor="center",
+            xanchor="left",
             x=0.5,
             font=dict(size=11, color="#4A5568"),
             itemsizing="constant",
